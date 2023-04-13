280c28aac70973dddeb8f663a6e377c32eae4413
Cole Milne <colemilne54@gmail.com>
2023-04-12 22:58:11 -0600

    helpers and such

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-import './style.css'
+import './style.css';
 
 import * as THREE from 'three';
 // need scene, camera, and renderer
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -19,10 +21,20 @@ renderer.render( scene, camera );
 
 // 3 steps to object: geometry (set of vectors), material (geometry wrapping paper, can use webgl for custom shaders), mesh (geometry + material)
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial ( { color: 0xFF6347, wireframe: true } );
+const material = new THREE.MeshStandardMaterial ( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
 
 function animate() {
   requestAnimationFrame( animate );
