d1a33ff9208d54b0c3589398af200533af156abf
Cole Milne <colemilne54@gmail.com>
2023-04-12 23:03:00 -0600

    onto stars

--- main.js
+++ main.js
@@ -36,6 +36,8 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
   requestAnimationFrame( animate );
 
@@ -43,6 +45,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  controls.update();
 
   renderer.render( scene, camera );
 };
