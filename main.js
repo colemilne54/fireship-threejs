ec3a7d33f53f79792c3bd1b3e8840ec86e4dffd6
Cole Milne <colemilne54@gmail.com>
2023-04-13 00:07:16 -0600

    try this

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ pointLight.position.set(20,20,20)
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
